ba70b5e029a8a6d7a5ca63eb16decec4d780a051
narayanj1193 <101072172+narayanj1193@users.noreply.github.com>
2022-05-12 09:32:19 +1200

    Add files via upload

--- 03_Bits_Calculator/00_bit_calc.py
+++ 03_Bits_Calculator/00_bit_calc.py
@@ -53,6 +53,30 @@ def user_choice():
             print ("Please choose a valid file type!")
             print()
 
+# checks input is a number more than a given value 
+def num_check(question, low):
+    valid = False
+    while not valid:
+        error = "Please enter an integer that is more than (or equal to {}) ".format(low)
+        
+        try:    
+            
+            # ask user to enter a number
+            response = int(input(question))
+
+            # checks number is more than zero
+            if response >= low:
+                return response  
+
+            # outputs error if input is invalid
+            else:
+                print(error)
+                print()
+        
+        except ValueError:
+            print(error)
+
+
 # Main Routine Goes Here
 
 # Heading
@@ -67,7 +91,18 @@ while keep_going == "":
     # Ask the user for the file type
     data_type = user_choice()
     print("You chose", data_type)
+
     # For integers, ask for integer
-    # (must be an integer more than / equal to 0)
+    if data_type =="integer":
+        var_integer = num_check("Enter an integer: ", 0)
 
     # For images ask for width and height
+    # (must be an integer more than / equal to 0)
+    elif data_type == "image":
+        image_width = num_check("Image width? ", 1)
+        print()
+        image_height = num_check("Image height? ", 1)
+
+    # For text, ask for a string
+    else:
+        var_text = input("Enter some text: ")
---
+++ 03_Bits_Calculator/03_integer_checker.py
@@ -0,0 +1,37 @@
+# checks input is a number more than zero
+def num_check(question, low):
+    valid = False
+    while not valid:
+        error = "Please enter an integer that is more than (or equal to {} ".format(low)
+        
+        try:    
+            
+            # ask user to enter a number
+            response = int(input(question))
+
+            # checks number is more than zero
+            if response >= low:
+                return response  
+
+            # outputs error if input is invalid
+            else:
+                print(error)
+                print()
+        
+        except ValueError:
+            print(error)
+
+# Main Routine goes here
+
+keep_going = ""
+while keep_going == "":
+    print()
+    # ask user for an integer (must be more than / equal to 0)
+    var_integer = num_check("Enter an integer: ", 0)
+    print()
+
+    # ask for width & height of an image
+    # (must be more than / equal to 1)
+    image_width = num_check("Image width? ", 1)
+    print()
+    image_height = num_check("Image height? ", 1)
